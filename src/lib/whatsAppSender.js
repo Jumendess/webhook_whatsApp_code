ee7ccc9330165eac82e452c860a263daee5a387a
Julio Mendes <101362282+Jumendess@users.noreply.github.com>
2025-01-28 19:03:49 -0300

    ajustes

--- src/lib/whatsAppSender.js
+++ src/lib/whatsAppSender.js
@@ -4,17 +4,19 @@ const log4js = require('log4js');
 const axios = require('axios');
 const fs = require('fs');
 const path = require('path');
-const mime = require('mime-to-extensions');
+const { Buffer } = require('buffer');
+const FormData = require('form-data');
+const mime = require('mime-types');
+require('dotenv').config();  // Certifique-se de carregar variáveis de ambiente
+
 let logger = log4js.getLogger('WhatsAppSender');
 logger.level = Config.LOG_LEVEL;
 
 /**
-* Queue, Dequeue and Send messages to Whatsapp
-*/
+ * Queue, Dequeue and Send messages to WhatsApp
+ */
 class WhatsAppSender {
-
     constructor() {
-
         this.messagesQueue = [];
         this.eventsEmitter = new Emitter();
         this.whatsAppApiUrl = Config.API_URL;
@@ -55,6 +57,7 @@ class WhatsAppSender {
                 self.messagesQueue.pop();
                 self.eventsEmitter.emit(Config.EVENT_PROCESS_NEXT_WHATSAPP_MESSAGE);
             });
+
         // Process next WhatsApp message from queue
         self.eventsEmitter.on(Config.EVENT_PROCESS_NEXT_WHATSAPP_MESSAGE,
             function () {
@@ -66,10 +69,10 @@ class WhatsAppSender {
     }
 
     /**
-    * Send Message to WhatsApp.
-    * @returns null
-    * @param {object} message - WhatsApp Message Payload to send.
-    */
+     * Send Message to WhatsApp.
+     * @returns null
+     * @param {object} message - WhatsApp Message Payload to send.
+     */
     async _sendMessageToWhatsApp(message) {
         let self = this;
         try {
@@ -77,12 +80,12 @@ class WhatsAppSender {
                 method: 'post',
                 url: `${self.whatsAppApiUrl}/${self.whatsAppApiVersion}/${self.whatsAppPhoneNumberId}/${self.whatsAppEndpointApi}`,
                 headers: {
-                  Authorization: `Bearer ${self.whatsAppAccessToken}`,
-                  'Content-Type': 'application/json'
+                    Authorization: `Bearer ${self.whatsAppAccessToken}`,
+                    'Content-Type': 'application/json'
                 },
                 data: message
             };
-            await axios(config).then( response => {
+            await axios(config).then(response => {
                 self.eventsEmitter.emit(Config.EVENT_WHATSAPP_MESSAGE_DELIVERED, response.data.messages[0].id);
             }).catch(function (error) {
                 throw new Error(error);
@@ -93,30 +96,30 @@ class WhatsAppSender {
     }
 
     /**
-    * Queue Message to be sent to WhatsApp.
-    * @returns null
-    * @param {object} message - WhatsApp Message payload.
-    */
+     * Queue Message to be sent to WhatsApp.
+     * @returns null
+     * @param {object} message - WhatsApp Message payload.
+     */
     _queueMessage(message) {
         let self = this;
         self.eventsEmitter.emit(Config.EVENT_QUEUE_MESSAGE_TO_WHATSAPP, message)
     }
 
     /**
-    * Remove message from cache after being delivered.
-    * @returns null
-    * @param {string} messageId - WhatsApp messageId that was delivered
-    */
+     * Remove message from cache after being delivered.
+     * @returns null
+     * @param {string} messageId - WhatsApp messageId that was delivered
+     */
     messageDelivered(messageId) {
         let self = this;
         self.eventsEmitter.emit(Config.EVENT_WHATSAPP_MESSAGE_DELIVERED, messageId);
     }
 
     /**
-    * Download and save attachment from WhatsApp
-    * @returns {string} fileName - file name
-    * @param {string} attachment - WhatsApp attachment information
-    */
+     * Download and save attachment from WhatsApp
+     * @returns {string} fileName - file name
+     * @param {string} attachment - WhatsApp attachment information
+     */
     async _downloadAndSaveWhatsAppAttachmentMessage(attachment) {
         try {
             let self = this;
@@ -139,14 +142,46 @@ class WhatsAppSender {
                 },
                 responseType: "stream"
             });
+
             const fileName = 'file_'.concat(Date.now()).concat('.').concat(mime.extension(attachment.mime_type));
-            await attachmentResponse.data.pipe(fs.createWriteStream(path.join(__dirname, '../../downloads/').concat(fileName)));
-            return fileName;
+            const localFilePath = path.join(__dirname, '../../downloads/', fileName);
+
+            // Save file locally
+            await attachmentResponse.data.pipe(fs.createWriteStream(localFilePath));
+
+            // Send file to WebDAV server
+            const webDAVUrl = process.env.ODA_WEBHOOK_URL // URL de upload configurada no .env
+            const credentials = {
+                username: process.env.WEB_DAV_USERNAME,
+                password: process.env.WEB_DAV_PASSWORD
+            };
+
+            const fileStream = fs.createReadStream(localFilePath);
+            const form = new FormData();
+            form.append('file', fileStream);
+
+            const uploadConfig = {
+                method: 'put',
+                url: `${webDAVUrl}/${fileName}`,  // URL de destino no servidor
+                headers: {
+                    ...form.getHeaders(),
+                    'Authorization': `Basic ${Buffer.from(credentials.username + ':' + credentials.password).toString('base64')}`,
+                },
+                data: form
+            };
+
+            // Upload file to WebDAV
+            const uploadResponse = await axios(uploadConfig);
+            console.log('File uploaded successfully:', uploadResponse.data);
+
+            // Return URL of file on WebDAV server
+            return `${webDAVUrl}/${fileName}`;
+
         } catch (error) {
             console.log(error);
             return null;
         }
     }
-
 }
+
 module.exports = WhatsAppSender;
